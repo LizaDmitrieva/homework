525883df4437c5e7929a665cb093bff46220783c
LizaDmitrieva <elizabetharkness@gmail.com>
2020-10-20 18:21:47 +0500

    Выполнила домашнюю работу №3..

---
+++ homework3/homework3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace homework3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значение x: ");
+            var x = double.Parse(Console.ReadLine());
+
+            var y = F(x);
+
+            Console.WriteLine("f(x)= " + y);
+            Console.ReadKey();
+        }
+
+        static double F(double x) 
+        {
+            return (Math.Pow(x, 2) + 10) / (Math.Sqrt(Math.Pow(x, 2) + 1));
+        }
+    }
+}
