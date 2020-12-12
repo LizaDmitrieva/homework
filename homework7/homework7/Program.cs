69f5784f046b9461b117dc2c41e5e066af4b8652
LizaDmitrieva <elizabetharkness@gmail.com>
2020-12-12 14:23:38 +0500

    Выполнено задание 8.1

---
+++ homework7/homework7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace homework7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значение x ");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Значение функции равно " + CalcFunctionResult(x));
+
+            Console.ReadKey();
+        }
+
+        public static double CalcFunctionResult(double x)        
+        {
+            if (2 < x)
+            {
+                return 2;
+            }
+            else 
+            {
+                if (-2 <= x && x <= 2)
+                {
+                    return x;
+                }
+                else
+                {
+                    return -2;
+                }
+            } 
+        }
+    }    
+}
