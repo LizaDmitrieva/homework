da6944892dea9f309ceb1516a1e6f1dadd87cddb
LizaDmitrieva <elizabetharkness@gmail.com>
2021-12-27 16:32:33 +0500

    выполнена домашняя работа №6 (ООП)

---
+++ PhotoEnhancer3/PhotoEnhancer/Filters/Parameters/RangeDecreasingParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoEnhancer
+{
+    public class RangeDecreasingParameters : IParameters
+    {
+        [ParameterInfo(Name = "Коэффициент 1", MinValue = 0, MaxValue = 1, DefaultValue = 0, Increment = 0.01)]
+        public double Coefficent1 { get; set; }
+
+        [ParameterInfo(Name = "Коэффициент 2", MinValue = 0, MaxValue = 1, DefaultValue = 1, Increment = 0.01)]       
+        public double Coefficent2 { get; set; }
+    }
+}
